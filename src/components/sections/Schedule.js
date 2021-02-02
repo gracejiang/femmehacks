5a5e0d049fd95f7e2a4d9000a3641c0da7907609
yuxin <yuxinl329@gmail.com>
2021-02-02 11:41:22 -0500

    update schedule and add logo

--- src/components/sections/Schedule.js
+++ src/components/sections/Schedule.js
@@ -8,53 +8,52 @@ import Emoji from "../common/emoji"
 const Schedule = () => (
   <Section id="schedule" accent>
     <Container>
-      <h1>2021 Schedule <Emoji symbol="🕒"/></h1>
-      <br/>
-      <p>You can view our schedule
-          in <b><a href="https://drive.google.com/file/d/1dD_zkTXmSyTQfCGfTFsH6y1_MK7EO442/view?usp=sharing">PDF form</a></b>. We
-        will also be emailing the finalized schedule with meeting links to all attendees before the event!</p>
-      <br/>
+      <h1>2021 Schedule <Emoji symbol="🕒" /></h1>
+      <br />
+      <p> All the <b>meeting links</b> are available on our Airtable -- please check the <b>Event Location</b> section the acceptance email to find the Airtable link!</p>
+      <br />
       <Row>
         <Col xs={12} md={6}>
           <h3>February 5 (Friday)</h3>
           <p>
-            <br/><b>3:00pm - 6:00pm</b>: <b>Check-in</b>
-            <br/><b>3:00pm - 4:30pm</b>: Introductions &amp; Team Formation on Discord
-            <br/><b>4:30pm - 5:00pm</b>: <b>Opening Ceremony</b>
-            <br/><b>5:00pm - 6:00pm</b>: Sponsors Meet n' Greet!
-            <br/><b>6:00pm - 6:45pm</b>: [Beginner] Intro to Git with Andrea Baric
-            <br/><b>6:00pm - 6:45pm</b>: [Intermediate] Intro to UI/UX with Sanjana
-            <br/><b>7:00pm - 7:45pm</b>: [Beginner] Intro to Web Dev with Hack4Impact
-            <br/><b>7:00pm - 8:00pm</b>: Cloud Hero Workshop Pt. 1 with Google Cloud
-            <br/><b>8:00pm - 8:45pm</b>: [Beginner] Intro to iOS with Steph Tang
-            <br/><b>8:00pm - 8:45pm</b>: [Intermediate] Intermediate Web Dev with Hack4Impact
-            <br/><b>8:00pm - 9:00pm</b>: Cloud Hero Workshop Pt. 2 with Google Cloud
-            <br/><b>9:00pm - 9:30pm</b>: Speed Dating &amp; Team Formation
-            <br/><b>9:30pm - 10:30pm</b>: Discord Game Night (Among Us, Code Names, &amp; more!)
+            <br /><b>3:00pm - 6:00pm</b>: <b>Check-in</b>
+            <br /><b>3:00pm - 4:30pm</b>: Introductions &amp; Team Formation on Discord
+            <br /><b>4:30pm - 5:00pm</b>: <b>Opening Ceremony</b>
+            <br /><b>5:00pm - 6:00pm</b>: Sponsors Meet n' Greet!
+            <br /><b>6:00pm - 6:45pm</b>: [Beginner] Intro to Git with Andrea Baric
+            <br /><b>6:00pm - 6:45pm</b>: [Intermediate] Intro to UI/UX with Sanjana
+            <br /><b>7:00pm - 7:45pm</b>: [Beginner] Intro to Web Dev with Hack4Impact
+            <br /><b>7:00pm - 8:00pm</b>: Cloud Hero Workshop Pt. 1 with Google Cloud
+            <br /><b>8:00pm - 8:45pm</b>: [Beginner] Intro to iOS with Steph Tang
+            <br /><b>8:00pm - 8:45pm</b>: [Intermediate] Intermediate Web Dev with Hack4Impact
+            <br /><b>8:00pm - 9:00pm</b>: Cloud Hero Workshop Pt. 2 with Google Cloud
+            <br /><b>9:00pm - 9:30pm</b>: Speed Dating &amp; Team Formation
+            <br /><b>9:30pm - 10:30pm</b>: Discord Game Night (Among Us, Code Names, &amp; more!)
           </p>
         </Col>
 
         <Col xs={12} md={6}>
           <h3>February 6 (Saturday)</h3>
           <p>
-            <br/><b>8:00am</b>: <b>Hacking Starts</b>
-            <br/><b>8:00am - 9:30am</b>: Office Hours – Ideation
-            <br/><b>9:30am - 10:30am</b>: Add SMS and Voice to your Apps with Twilio
-            <br/><b>10:30am - 11:30am</b>: Facebook Panel and Q &amp; A
-            <br/><b>11:00am - 12:00pm</b>: Start-up Workshop with Krish
-            <br/><b>12:00pm - 1:00pm</b>: Women in Tech Panel!
-            <br/><b>1:00pm - 2:00pm</b>: How to Ace Tech Interviews with Vanguard
-            <br/><b>2:00pm - 4:00pm</b>: Skribbl
-            <br/><b>2:00pm - 3:00pm</b>: Workout session with Urban Outfitters
-            <br/><b>2:00pm - 2:30pm</b>: The Percentage Project: Data-Driven Advocacy
-            <br/><b>3:00pm - 4:00pm</b>: Product Management 101 with Microsoft PM Andrea Baric
-            <br/><b>4:00pm - 4:30pm</b>: Venture Capital Workshop with Wiress
-            <br/><b>6:00pm - 8:00pm</b>: Office Hours
-            <br/><b>8:00pm</b>: <b>Hacking Stops</b>
-            <br/><b>8:30pm</b>: <b>Devpost Submission Deadline</b>
-            <br/><b>8:45pm - 10:00pm</b>: Judging Time
-            <br/><b>9:30pm - 10:00pm</b>: Trivia Night
-            <br/><b>8:00pm</b>: <b>Closing Ceremony</b>
+            <br /><b>8:00am</b>: <b>Hacking Starts</b>
+            <br /><b>8:00am - 9:30am</b>: Office Hours – Ideation
+            <br /><b>9:30am - 10:30am</b>: Add SMS and Voice to your Apps with Twilio
+            <br /><b>10:30am - 11:30am</b>: Facebook Panel and Q &amp; A
+            <br /><b>11:00am - 12:00pm</b>: Start-up Workshop with Krish
+            <br /><b>11:30am - 12:00pm</b>: Soulsmiles Workshop
+            <br /><b>12:00pm - 1:00pm</b>: Women in Tech Panel!
+            <br /><b>1:00pm - 2:00pm</b>: How to Ace Tech Interviews with Vanguard
+            <br /><b>2:00pm - 4:00pm</b>: Skribbl on Discord
+            <br /><b>2:00pm - 3:00pm</b>: Workout session with Urban Outfitters
+            <br /><b>2:00pm - 2:30pm</b>: The Percentage Project: Data-Driven Advocacy
+            <br /><b>3:00pm - 4:00pm</b>: Product Management 101 with Microsoft PM Andrea Baric
+            <br /><b>4:00pm - 4:30pm</b>: Venture Capital Workshop with Wiress
+            <br /><b>6:00pm - 8:00pm</b>: Office Hours
+            <br /><b>8:00pm</b>: <b>Hacking Stops</b>
+            <br /><b>8:30pm</b>: <b>Devpost Submission Deadline</b>
+            <br /><b>8:45pm - 9:30pm</b>: Judging Time
+            <br /><b>9:00pm - 9:30pm</b>: Trivia Night!
+            <br /><b>9:40pm</b>: <b>Closing Ceremony</b>
           </p>
         </Col>
       </Row>
